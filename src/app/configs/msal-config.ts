4d7d0de729fbce94ba2bb632237a16837ed3bf74
Andrew Yupin <brolo1341@gmail.com>
2025-02-25 11:45:38 +0200

    update MSAL configuration to set clientId for OneDrive integration

--- src/app/configs/msal-config.ts
+++ src/app/configs/msal-config.ts
@@ -3,6 +3,7 @@ import { environment } from './../../environments/environment';
 export const msalConfig = {
   auth: {
     clientId: environment.clientId, // spa
+    // authority: 'https://login.microsoftonline.com/8395f3ee-eeb0-4cd6-b1c6-0731ab1fa9c9',
     redirectUri: 'http://localhost:4200',
   },
   cache: {
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: false,
-    clientId: '',
+    clientId: 'eb867d55-af5c-4b7b-864e-095244c0e528',
     oneDriveApi: 'https://graph.microsoft.com/v1.0/me/drive/root/children',
   };
